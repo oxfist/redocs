7e96666a5772e5713e5b5048fb423199f283cfcf
Andrés Quilodrán <andres.caro.q@gmail.com>
2022-06-11 16:57:15 -0400

    Tweak default style, config data and reformat with prettier

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,24 @@
-import React from 'react';
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import React from "react";
+import clsx from "clsx";
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import Layout from "@theme/Layout";
+import HomepageFeatures from "@site/src/components/HomepageFeatures";
 
-import styles from './index.module.css';
+import styles from "./index.module.css";
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+    <header className={clsx("hero hero--primary", styles.heroBanner)}>
       <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
+        <h1 className={clsx("hero__title", "redocs")}>{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
+            to="/docs/intro"
+          >
             ¡Quiero ver! 🚀
           </Link>
         </div>
@@ -27,11 +28,12 @@ function HomepageHeader() {
 }
 
 export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
       title={`Holi bbs ⚡️`}
-      description="Consigue grandes herramientas de desarrollo, fácilmente a tu disposición">
+      description="Las mejores herramientas para desarrollo web, fácilmente a tu disposición"
+    >
       <HomepageHeader />
       <main>
         <HomepageFeatures />
