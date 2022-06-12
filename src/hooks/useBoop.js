29aa0f32018fed2364d1f02fb93f0004e0308810
Andrés Quilodrán <andres.caro.q@gmail.com>
2022-06-12 13:46:13 -0400

    feat(home): add animations

---
+++ src/hooks/useBoop.js
@@ -0,0 +1,52 @@
+import { useCallback, useEffect, useState } from "react";
+import { useSpring } from "react-spring";
+
+/* It has default animation configurations */
+function useBoop({
+  x = 0,
+  y = 0,
+  rotation = 0,
+  scale = 1,
+  timing = 150,
+  springConfig = {
+    tension: 300,
+    friction: 10,
+  },
+}) {
+  const [isBooped, setIsBooped] = useState(false);
+  const style = useSpring({
+    display: "inline-block",
+    backfaceVisibility: "hidden",
+    transform: isBooped
+      ? `translate(${x}px, ${y}px)
+         rotate(${rotation}deg)
+         scale(${scale})`
+      : `translate(0px, 0px)
+         rotate(0deg)
+         scale(1)`,
+    config: springConfig,
+  });
+
+  useEffect(() => {
+    if (!isBooped) {
+      return;
+    }
+
+    // Always cancel animation after timing duration
+    const timeoutId = window.setTimeout(() => {
+      setIsBooped(false);
+    }, timing);
+
+    return () => {
+      window.clearTimeout(timeoutId);
+    };
+  }, [isBooped, timing]);
+
+  const trigger = useCallback(() => {
+    setIsBooped(true);
+  }, []);
+
+  return [style, trigger];
+}
+
+export default useBoop;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,25 +1,70 @@
-import React from "react";
+import React, { useEffect } from "react";
 import clsx from "clsx";
+import { useSpring, animated } from "react-spring";
+
 import Link from "@docusaurus/Link";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Layout from "@theme/Layout";
 import HomepageFeatures from "@site/src/components/HomepageFeatures";
 
+import useBoop from "../hooks/useBoop";
+
 import styles from "./index.module.css";
 
+const MAIN_HEADER_SPRING_CONFIG = {
+  to: { opacity: 1, y: 0 },
+  from: { opacity: 0, y: 60 },
+  config: {
+    tension: 300,
+    friction: 20,
+  },
+};
+const ROCKET_SPRING_CONFIG = {
+  x: 5,
+  y: -5,
+};
+
 function HomepageHeader() {
+  const headerAnimationConfig = useSpring(MAIN_HEADER_SPRING_CONFIG);
+  const [arrowAnimationConfiguration, triggerBoop] =
+    useBoop(ROCKET_SPRING_CONFIG);
+
+  useEffect(() => {
+    let intervalId;
+    const timeoutId = window.setTimeout(() => {
+      triggerBoop();
+
+      intervalId = window.setInterval(() => {
+        triggerBoop();
+      }, 5000);
+    }, 2000);
+
+    return () => {
+      window.clearTimeout(timeoutId);
+      window.clearInterval(intervalId);
+    };
+  }, [triggerBoop]);
+
   const { siteConfig } = useDocusaurusContext();
   return (
     <header className={clsx("hero hero--primary", styles.heroBanner)}>
       <div className="container">
-        <h1 className={clsx("hero__title", "redocs")}>{siteConfig.title}</h1>
+        <animated.h1
+          style={headerAnimationConfig}
+          className={clsx("hero__title", "redocs")}
+        >
+          {siteConfig.title}
+        </animated.h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro"
           >
-            ¡Quiero ver! 🚀
+            ¡Quiero ver!{" "}
+            <animated.span style={arrowAnimationConfiguration}>
+              🚀
+            </animated.span>
           </Link>
         </div>
       </div>
